248b03f5a4fafe4dad861a7421ead3aa352c21e9
Amateratsu888 <vendell.cg@gmail.com>
2021-09-11 19:10:44 +0100

    Cheikhpoint Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Profile from './profile/Profile'
+import Photo from './profile/pp.jpg'
 function App() {
+  const handleName = () => {alert("yoooooooo")}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Profile >
+     
+     </Profile>
+    </>
   );
 }
 
---
+++ src/profile/Profile.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+const Profile = (props)=> {
+
+
+        return (
+            <>
+                 <div className="container mt-5 d-flex justify-content-center">
+                    <div className="card p-3" style={card}>
+                        <div className="d-flex align-items-center">
+                            <div className="image">{props.children}</div>
+                            <div className="ml-3 w-100">
+                               {props.fullName}  {props.profession}<div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">{props.bio}
+                                </div>
+                                <div className="button mt-2 d-flex flex-row align-items-center justify-content-between"> <button onClick={props.handleName} className="btn btn-sm btn-primary w-100">Handle Name</button>  </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+Profile.defaultProps = {
+    fullName : "Naruto Usumaki",
+    profession : "Hokage",
+    bio : "C'est le gnomy au lait qui est bon ",
+    handleName : () => {alert("Ta pas mis de props Tu t'atendais a quoi?!")},
+    children : <img src='/default.png' style={{height:150,width:150,marginRight:10}}/>
+}
+
+const card = {
+    width: 400,
+    border: null,
+    borderRadius: 10,
+    backgroundColor: '#fff'
+}
+
+export default Profile
